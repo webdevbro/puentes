a14d542e7f6492fc165794f5f34e9ed433e69b69
Ronaldo Ortiz <ronyortizop@hotmail.com>
2021-07-15 22:41:06 -0400

    finished video functionality, added original images and videos, cleand up

--- src/pages/Fullpage.js
+++ src/pages/Fullpage.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
 import TopMenu from "../components/TopMenu";
-import Flowers from "../assets/img/flowers.mp4";
+import { Button, IconButton } from "@material-ui/core";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import PuentesVideo1 from "../assets/img/vids/puentes1.mp4";
+import PuentesVideo2 from "../assets/img/vids/puentes2.mp4";
+import PuentesLogo from "../assets/img/puentes-logo-white.png";
 import "./Fullpage.css";
 
 const Fullpage = () => (
@@ -18,7 +22,37 @@ const Fullpage = () => (
       render={({ state, fullpageApi }) => {
         return (
           <ReactFullpage.Wrapper>
-            <div className="section" id="section3">
+            <div
+              className="section section1"
+              style={{ backgroundColor: "#000000" }}
+            >
+              <div className="logoIntroCont">
+                <div className="logoIntro">
+                  <img
+                    src={PuentesLogo}
+                    alt="Puentes - Conectando Oportunidades para Jóvenes"
+                  />
+                </div>
+                {/* <Button
+                  variant="contained"
+                  color="success"
+                  onClick={() => fullpageApi.moveSectionDown()}
+                >
+                  Go down!
+                </Button> */}
+                <IconButton
+                  aria-label="go down!"
+                  onClick={() => fullpageApi.moveSectionDown()}
+                >
+                  <ArrowDownwardIcon
+                    style={{ color: "#FFFFFF", fontSize: "3rem" }}
+                  />
+                </IconButton>
+              </div>
+            </div>
+
+            {/* SECTION 2 - VIDEO */}
+            <div className="section" id="section2">
               <video
                 data-autoPlay
                 id="myVideo"
@@ -34,7 +68,7 @@ const Fullpage = () => (
                   zIndex: "-1",
                 }}
               >
-                <source src={Flowers} type="video/mp4" />
+                <source src={PuentesVideo2} type="video/mp4" />
                 {/* <source
               src="./assets/images/bgpics/flowers.webm"
               type="video/webm"
@@ -44,29 +78,21 @@ const Fullpage = () => (
                 <h1>fullPage.js videos</h1>
               </div>
             </div>
+
             <div
-              className="section section0"
-              style={{ backgroundColor: "#000000" }}
-            >
-              <h3>First section</h3>
-              <button onClick={() => fullpageApi.moveSectionDown()}>
-                Go down!
-              </button>
-            </div>
-            <div
-              className="section section1"
-              style={{ backgroundColor: "#000000" }}
-            >
-              <h3>Second section</h3>
-              <button onClick={() => fullpageApi.moveSectionDown()}>
-                Go down!
-              </button>
-            </div>
-            <div
-              className="section section2"
+              className="section section3"
               style={{ backgroundColor: "#000000" }}
             >
               <h3>Third section</h3>
+              <button onClick={() => fullpageApi.moveSectionDown()}>
+                Go down!
+              </button>
+            </div>
+            <div
+              className="section section4"
+              style={{ backgroundColor: "#000000" }}
+            >
+              <h3>Fourth section</h3>
               <button onClick={() => fullpageApi.moveSectionDown()}>
                 Go down!
               </button>
